52c269d8715ca6fb580f340ce19dcbff91c8b386
ameted83 <tedeschiamelia@gmail.com>
2022-07-09 11:41:00 +0200

    Integrate Prisma client with mocking

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,14 +1,40 @@
 import supertest from "supertest";
 
+import { prismaMock } from "./lib/prisma/client.mock";
+
 import app from "./app";
 
 const request = supertest(app);
 
 test("GET /planets", async () => {
+    const planets = [
+        {
+            id: 1,
+            name: "Mercury",
+            description: null,
+            diameter: 1234,
+            moons: 12,
+            createdAt: "2022-07-09T07:58:16.615Z",
+            updatedAt: "2022-07-09T08:00:07.579Z",
+        },
+        {
+            id: 2,
+            name: "Venus",
+            description: null,
+            diameter: 5678,
+            moons: 3,
+            createdAt: "2022-07-09T07:59:48.447Z",
+            updatedAt: "2022-07-09T08:02:42.395Z",
+        },
+    ];
+
+    // @ts-ignore //
+    prismaMock.planet.findMany.mockResolvedValue(planets);
+
     const response = await request
         .get("/planets")
         .expect(200)
         .expect("Content-Type", /application\/json/);
 
-    expect(response.body).toEqual([{ name: "Mercury" }, { name: "Venus" }]);
+    expect(response.body).toEqual(planets);
 });
